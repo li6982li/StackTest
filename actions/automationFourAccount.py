a0afbea730fa52de4750b7c2a6455f6ffe42e1bd
lyn <649041632@qq.com>
2022-05-26 10:22:51 +0800

    6

--- actions/automationFourAccount.py
+++ actions/automationFourAccount.py
@@ -3,7 +3,9 @@ from st2common.runners.base_action import Action
 
 class Ticket(Action):
     def run(self,monitorTicket):
-        monitorTicket = eval(monitorTicket)
+        if isinstance(monitorTicket,str):
+            monitorTicket = eval(monitorTicket)
+
         if monitorTicket['Priority'] == "Critical":
             if monitorTicket['Submitter'] in ['niulx2', 'tiandj1', 'chenlei16', 'shihy4', 'huangjie5', 'wahmad']:
                 return ""
