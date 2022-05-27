85d66ab2af3f241ee5f868cecf910da303e22d03
lyn <649041632@qq.com>
2022-05-27 17:02:17 +0800

    9

--- actions/automationFourAccount.py
+++ actions/automationFourAccount.py
@@ -3,18 +3,24 @@ from st2common.runners.base_action import Action
 
 class Ticket(Action):
     def run(self,monitorTicket):
-        if isinstance(monitorTicket,str):
-            monitorTicket = eval(monitorTicket)
+        try:
+            print(monitorTicket)
+            print(type(monitorTicket))
+            if isinstance(monitorTicket,str):
+                monitorTicket = eval(monitorTicket)
 
-        if monitorTicket['Priority'] == "Critical":
-            if monitorTicket['Submitter'] in ['niulx2', 'tiandj1', 'chenlei16', 'shihy4', 'huangjie5', 'wahmad']:
+            if monitorTicket['Priority'] == "Critical":
+                if monitorTicket['Submitter'] in ['niulx2', 'tiandj1', 'chenlei16', 'shihy4', 'huangjie5', 'wahmad']:
+                    return ""
+                return "P1"
+            if monitorTicket.get('Reported Source') == "Monitor" and \
+                    (monitorTicket.get("Assignee") in ["automation_i", "automation_a", "automation_b", "automation_p"] or
+                     (monitorTicket.get("Assigned Group") == "Automation" and monitorTicket.get(
+                         'Assigned Organization') == "CC-Monitoring" and monitorTicket.get("Assignee") == "auto_fix"
+                     )):
+                return "auto"
+            else:
                 return ""
-            return "P1"
-        if monitorTicket.get('Reported Source') == "Monitor" and \
-                (monitorTicket.get("Assignee") in ["automation_i", "automation_a", "automation_b", "automation_p"] or
-                 (monitorTicket.get("Assigned Group") == "Automation" and monitorTicket.get(
-                     'Assigned Organization') == "CC-Monitoring" and monitorTicket.get("Assignee") == "auto_fix"
-                 )):
-            return "auto"
-        else:
-            return ""
+        except Exception as e:
+            print("error:%s" % e)
+            return e
