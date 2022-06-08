b67513760f686e880e93f071d60eeb7852d1f922
lyn <649041632@qq.com>
2022-06-08 12:09:24 +0800

    23

--- actions/automationP1Notice.py
+++
@@ -1,8 +0,0 @@
-def
-
-if dMsg['Submitter'] in ['niulx2', 'tiandj1', 'chenlei16', 'shihy4', 'huangjie5', 'wahmad']:
-    return None
-from automationP1 import SmsTtsCallP1
-
-call_obj = SmsTtsCallP1()
-call_obj.ali_call_number(dMsg['Incident ID'], "01057870297")
